6f013b41bbfe1d95faeea2c32d4f5875cfe11d27
user-friendly <pivanov@foobar.ninja>
2021-07-01 07:56:13 -0400

    Add process debug info

--- app.js
+++ app.js
@@ -15,6 +15,26 @@ const app = express()
 const port = 80
 const portSsl = 443
 
+if (process.env.NODE_ENV === 'development') {
+    let debugInfo = `
+Process information:
+    NODE_ENV:           ${process.env.NODE_ENV},
+    
+    ports:              [${port}, ${portSsl}],
+    node_args:          ${process.env.node_args},
+    
+    username:           ${process.env.username},
+    watch:              ${process.env.watch},
+    ignore_watch:       '${process.env.ignore_watch}',
+    
+    pm_pid_path:        ${process.env.pm_pid_path},
+    pm_err_log_path:    ${process.env.pm_err_log_path},
+    pm_out_log_path:    ${process.env.pm_out_log_path},
+`
+    console.log(debugInfo)
+}
+
+
 let httpsOptions = {
   key: fs.readFileSync('./sslcert/calcalc.key'),
   cert: fs.readFileSync('./sslcert/calcalc.crt')
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,22 +3,16 @@ module.exports = {
     name:           "calcalc",
     script:         "./app.js",
     watch:          true,
-    // What does this exactly do?
-    // watch_delay:    250,
+    ignore_watch:   ['pm2', 'node_module', 'public', 'logs'],
     
     exp_backoff_restart_delay: 100,
-    autorestart:    false,
-    
-    interpreter_args: "--inspect",
     
     pid_file:       "./pm2/app.pid",
     error_file:     "./pm2/app_error.log",
     out_file:       "./pm2/app_out.log",
     
-    // FIXME Does not get applied when calling pm2 --env development
-    env_development: {
+    env: {
         NODE_ENV:     "development",
-        interpreter_args: "--inspect",
     },
     env_test: {
         NODE_ENV:     "test",
